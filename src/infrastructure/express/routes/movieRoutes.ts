096c0031c5ac2c9258629387e35282b39efb95fc
Jonatha Rodrigo Follmer <jonatha.follmer@compasso.com.br>
2024-05-17 18:03:22 -0300

    fix: fix controller params at router

--- src/infrastructure/express/routes/movieRoutes.ts
+++ src/infrastructure/express/routes/movieRoutes.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { MovieController } from "../../../adapters/controllers/movieController";
 import { MovieApiServiceMock } from "../../../adapters/external-services/movieApiServiceMock";
+import { TMDBApiExternalService } from "../../../adapters/external-services/tmdbService";
 import { MovieServiceImpl } from "../../../usecases/movieServiceMLImp";
+import { MovieByTitleServiceImpl } from "../../../usecases/movieByTitleServiceImp";
 
 const router = express.Router();
+
 const movieApiService = new MovieApiServiceMock()
-const movieService = new MovieServiceImpl(movieApiService);
-const movieController = new MovieController(movieService);
+const movieMLService = new MovieServiceImpl(movieApiService);
+
+const externalTMDBAPIService = new TMDBApiExternalService();
+const tmdbAPIService = new MovieByTitleServiceImpl(externalTMDBAPIService);
+
+const movieController = new MovieController(movieMLService, tmdbAPIService);
 
 router.get("/", movieController.recommendMovies.bind(movieController));
 
