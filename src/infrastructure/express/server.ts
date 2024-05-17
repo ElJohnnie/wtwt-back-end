28ffb844cc204c58393ee70cb88a008c4aa7e6a4
Jonatha Rodrigo Follmer <jonatha.follmer@compasso.com.br>
2024-05-17 19:51:16 -0300

    fix: insert dotenv

--- src/infrastructure/express/server.ts
+++ src/infrastructure/express/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import movieRoutes from "./routes/movieRoutes";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
