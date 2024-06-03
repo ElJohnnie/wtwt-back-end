e3064f571292fafc009ab6fec4282a33c3684436
Jonatha Rodrigo Follmer <jonatha.follmer@compasso.com.br>
2024-06-02 21:28:59 -0300

    fix: fix middlewares path

--- src/infrastructure/express/server.ts
+++ src/infrastructure/express/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import movieRoutes from "./routes/movieRoutes";
 import corsConfig from './config/corsConfig';
-import checkAuthorizationCode from './middlwares/checkAuthorizationCode';
+import checkAuthorizationCode from './middlewares/checkAuthorizationCode';
 
 const app = express();
 
