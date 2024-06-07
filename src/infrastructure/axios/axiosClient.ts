ce1608ca7a0e3c841dfa429dba209d571d1c70c3
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-06-07 14:46:51 -0300

    fix: change axiosClient to Factory pattern

--- src/infrastructure/axios/axiosClient.ts
+++ src/infrastructure/axios/axiosClient.ts
@@ -1,25 +1,27 @@
 import axios, { AxiosInstance } from 'axios';
 
 export class AxiosClient {
-    private static instance: AxiosInstance;
+    private static instances: Map<string, AxiosInstance> = new Map();
 
     static getInstance(apiBaseUrl: string): AxiosInstance {
-        if (!AxiosClient.instance) {
+        if (!this.instances.has(apiBaseUrl)) {
             const token = process.env.TMDB_API_TOKEN;
 
             if (!token) {
                 throw new Error("Token da API TMDB não fornecido. Defina a variável de ambiente TMDB_API_TOKEN.");
             }
 
-            AxiosClient.instance = axios.create({
+            const instance = axios.create({
                 baseURL: apiBaseUrl,
                 headers: {
                     accept: 'application/json',
                     Authorization: `Bearer ${token}`
                 }
             });
+
+            this.instances.set(apiBaseUrl, instance);
         }
 
-        return AxiosClient.instance;
+        return this.instances.get(apiBaseUrl)!;
     }
 }
