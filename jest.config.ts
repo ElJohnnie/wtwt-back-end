938733bdff9f09cc9754e1bf812c9eda57468fd6
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-07-15 23:41:23 -0300

    config: jest coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
 };
