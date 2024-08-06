86364110e3a9d52feab55c9cb0cbc7363b2c2174
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-08-06 15:38:13 -0300

    fix: remove temporaly the randomic choice

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['html', 'text'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/main.{js,jsx,ts,tsx}',
+    '!src/infrastructure/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
   coverageThreshold: {
     global: {
       statements: 90,
