e62d22a1b8e2bb353d8951150ab2c7ff8114b487
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-09-23 20:31:06 -0300

    feat: make to random choise

--- tests/src/application/usecases/movieServiceMLImp.spec.ts
+++ tests/src/application/usecases/movieServiceMLImp.spec.ts
@@ -9,7 +9,7 @@ const movieService = new MovieServiceImpl(mockMovieApiService);
 
 describe('MovieServiceImpl', () => {
     beforeEach(() => {
-        jest.clearAllMocks()
+        jest.clearAllMocks();
     });
 
     it('should return processed recommended movies', async () => {
@@ -22,6 +22,63 @@ describe('MovieServiceImpl', () => {
 
         const recommendedMovies = await movieService.getRecommendedMovies(movieData);
 
-        expect(recommendedMovies).toEqual([{ title: 'Movie 1' }, { title: 'Movie 2' }]);
+        expect(recommendedMovies).toEqual(expect.any(Array));
+        expect(recommendedMovies[0]).toHaveProperty('title');
+    });
+
+    it('should call triggerML with correct movie data', async () => {
+        const movieData: Movie = {
+            mood: 'happy',
+            primaryGenre: 'Comedy',
+            secondaryGenre: 'Romance',
+            epoch: '2000'
+        };
+
+        await movieService.getRecommendedMovies(movieData);
+
+        expect(mockMovieApiService.triggerML).toHaveBeenCalledWith(movieData);
+    });
+
+    it('should process recommended movies correctly', async () => {
+        const movieData: Movie = {
+            mood: 'excited',
+            primaryGenre: 'Action',
+            secondaryGenre: 'Adventure',
+            epoch: '2020'
+        };
+
+        const recommendedMovies = await movieService.getRecommendedMovies(movieData);
+
+        expect(recommendedMovies[0]).toEqual(expect.objectContaining({ title: expect.any(String) }));
+    });
+
+    it('should randomize choice correctly when there is more than one movie', async () => {
+        const movieData: Movie = {
+            mood: 'bored',
+            primaryGenre: 'Drama',
+            secondaryGenre: 'Mystery',
+            epoch: '1990'
+        };
+
+        const recommendedMovies = await movieService.getRecommendedMovies(movieData);
+
+        expect(recommendedMovies.length).toBe(1);
+        expect(['Movie 1', 'Movie 2']).toContain(recommendedMovies[0].title);
+    });
+
+    it('should return the only movie when there is one movie', async () => {
+        mockMovieApiService.triggerML.mockResolvedValueOnce([{ title: 'Only Movie' }]);
+
+        const movieData: Movie = {
+            mood: 'neutral',
+            primaryGenre: 'Documentary',
+            secondaryGenre: 'Biography',
+            epoch: '2015'
+        };
+
+        const recommendedMovies = await movieService.getRecommendedMovies(movieData);
+
+        expect(recommendedMovies.length).toBe(1);
+        expect(recommendedMovies[0].title).toBe('Only Movie');
     });
 });
