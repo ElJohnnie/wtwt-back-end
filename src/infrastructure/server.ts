527f33955801dcc4a2fa21ccee90d8b3c8c77e17
Jonatha Follmer <johnfollmer1995@gmail.com>
2025-06-16 09:20:19 -0300

    feat: changed express params and added helme to security improvment

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -4,10 +4,15 @@ import movieRoutes from "./routes/movies.routes";
 import corsConfig from './config/cors.config';
 import checkAuthorizationCode from './middlewares/check-authorization-code.middleware';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use(cors(corsConfig));
 
